c327987068b9804da9ff0c55f4daaa48e1f0827c
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2024-06-12 15:18:46 -0300

    create app storage

---
+++ lib/utils/app_storage.dart
@@ -0,0 +1,31 @@
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+class AppStorage {
+  final Box _box;
+
+  AppStorage(Box box): _box = box;
+
+  static Future<AppStorage> init() async {
+    final documentDirectory = await getApplicationDocumentsDirectory();
+    Hive.init(documentDirectory.path);
+    Box box = await Hive.openBox('game-stats');
+    return AppStorage(box);
+  }
+
+  String? getByKey(String key) {
+    return _box.get(key);
+  }
+
+  void setByKey(String key, String value) {
+    _box.put(key, value);
+  }
+
+  void removeByKey(String key) {
+    _box.delete(key);
+  }
+
+  void clear() {
+    _box.clear();
+  }
+}
